a6688d67ff26c876f36693cf8bd01ecdb146d6b3
nazm0l <tanjirahmednazmul@gmail.com>
2022-04-16 03:00:29 +0600

    user verification and name added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,54 +1,93 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import auth from '../../../firebase.init'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { async } from "@firebase/util";
 
 const Register = () => {
+  const [agree, setAgree] = useState(false);
 
-    const [
-        createUserWithEmailAndPassword,
-        user,
-        loading,
-        error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, {sendEmailVerification:true});
 
-    const navigate = useNavigate();
+    const [updateProfile, updating, errorProfile] = useUpdateProfile(auth);
 
-    const navigateLogin = () =>{
-        navigate('/login')
-    }
+  const navigate = useNavigate();
 
-    if(user){
-        navigate('/');
-    }
+  const navigateLogin = () => {
+    navigate("/login");
+  };
 
-    const handleRegister = event => {
-        event.preventDefault();
-        const name = event.target.name.value;
-        const email = event.target.email.value;
-        const password =event.target.password.value;
+//   if (user) {
+//     navigate("/");
+//   }
 
-        createUserWithEmailAndPassword(email, password);
-    }
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
-    return (
-        <div className='container w-25 mx-auto'>
-            <h2 className='text-center fw-bold mt-5'>Register Now</h2>
-            <form onSubmit={handleRegister}>
-                <input className='mt-2 w-100 py-2' type="text" name="name" id="" placeholder='Your Name'/>
-                <br />
-                <input className='mt-2 w-50 w-100 py-2' type="email" name="email" id="" placeholder='Email' required/>
-                <br />
-                <input className='mt-2 w-50 w-100 py-2' type="password" name="password" id="" placeholder='Password' required />
-                <br />
-                <input className='mt-3' type="checkbox" name="terms" id="" />
-                <label className='ms-2' htmlFor="terms">Terms and Conditions</label>
-                <br />
-                <input className='btn btn-dark mt-2' type="submit" value="Register" />
-            </form>
-            <p className="mt-3">Already Registerd? <Link to="/login" onClick={navigateLogin} className="text-danger text-decoration-none">Login Here</Link></p>
-        </div>
-    );
+    await createUserWithEmailAndPassword(email, password);
+    await updateProfile({displayName: name});
+    navigate("/");
+  };
+
+  return (
+    <div className="container w-25 mx-auto">
+      <h2 className="text-center fw-bold mt-5">Register Now</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          className="mt-2 w-100 py-2"
+          type="text"
+          name="name"
+          id=""
+          placeholder="Your Name"
+        />
+        <br />
+        <input
+          className="mt-2 w-50 w-100 py-2"
+          type="email"
+          name="email"
+          id=""
+          placeholder="Email"
+          required
+        />
+        <br />
+        <input
+          className="mt-2 w-50 w-100 py-2"
+          type="password"
+          name="password"
+          id=""
+          placeholder="Password"
+          required
+        />
+        <br />
+        <input
+          onClick={() => setAgree(!agree)}
+          className="mt-3"
+          type="checkbox"
+          name="terms"
+          id=""
+        />
+        <label className={`ps-2 ${agree? '' : 'text-danger'}`} htmlFor="terms">
+          Terms and Conditions
+        </label>
+        <br />
+        <input className={`btn btn-dark mt-2 ${agree? 'd-block' : 'disabled'}`} type="submit" value="Register" />
+      </form>
+      <p className="mt-3">
+        Already Registered?{" "}
+        <Link
+          to="/login"
+          onClick={navigateLogin}
+          className="text-danger text-decoration-none"
+        >
+          Login Here
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
